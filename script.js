28f93a982bef903cd89184c321438acbe669e02d
Yuhong <yuhongz9309@hstat.org>
2021-04-18 16:52:33 -0400

    add text warning for both inputs and remove alerts

--- script.js
+++ script.js
@@ -3,45 +3,48 @@ var hexadecimalInput;
 var pattern;
 var color1;
 var color2;
-const re = /[0-9A-Fa-f]{2}/g;
+const re = /[0-9A-Fa-f]{12}/g;
 
 jQuery(document).ready(function($) {
+    
     $("#binarySubmit").click(function() {
+        $("#binaryWarning1").hide();
+        $("#binaryWarning2").hide();
         binaryInput = $("#binaryInput").val();
         if (binaryInput.length == 4) {
             for (var i = 0; i < binaryInput.length; i++) {
                 if (binaryInput.substring(i,i+1) != 0 && binaryInput.substring(i,i+1) != 1) {
-                    alert("Please enter in binary only!");
+                    $("#binaryWarning1").show();
                     return false;
                 }
             }
             binaryInput = parseInt(binaryInput,2);
-            alert("YES " + binaryInput);
             $("#binaryInput").prop("disabled", true);
             $("#binarySubmit").hide();
         } else {
-            alert("Please enter an 4 bit binary!");
+             $("#binaryWarning2").show();
         }
     })
     $("#hexadecimalSubmit").click(function() {
+        $("#hexadecimalWarning1").hide();
+        $("#hexadecimalWarning2").hide();
         hexadecimalInput = $("#hexadecimalInput").val();
-        if(re.test(hexadecimalInput)) {
-            alert('valid hex' + hexadecimalInput);
-            $("#hexadecimalInput").prop("disabled", true);
-            $("#hexadecimalSubmit").hide();
-            color1 = "red"; // #"+ inputColor.substring(0,6);
-            color2 = "black"; //#"+ inputColor.substring(6,12);
-        } else {
-            if (hexadecimalInput.length == 12) {
-                alert("Please enter in hexadecimal only!");
+        if (hexadecimalInput.length == 12) {
+            if(re.test(hexadecimalInput)) {
+                $("#hexadecimalInput").prop("disabled", true);
+                $("#hexadecimalSubmit").hide();
+                color1 = "red"; // #"+ inputColor.substring(0,6);
+                color2 = "black"; //#"+ inputColor.substring(6,12);
             } else {
-                alert("Please enter a 12 hexadecimal string");
+                $("#hexadecimalWarning1").show();
             }
+        } else {
+            $("#hexadecimalWarning2").show();
         }
+        
     })
     $("#patternSubmit").click(function() {
         pattern = $("#patternOption").val();
-        alert(pattern);
         if (pattern == 1) {
             checkerBoard();
         } else if (pattern == 2) {
